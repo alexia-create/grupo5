f10806f70a1409eee62140cb2dd63527c1fbc763
ALEXIA_PIETRA <alexia.prado@aluno.uepb.edu.br>
2026-07-15 10:31:51 -0300

    feat: definir taxas de conversão de unidades de massa

---
+++ index.js
@@ -0,0 +1,9 @@
+// Conversor de Unidades de Massa
+
+const conversao_em_grama = {
+    g: 1,           // Grama
+    kg: 1000,       // Quilograma
+    lb: 453.592,    // Libra
+    oz: 28.3495,    // Onça
+    mg: 0.001       // Miligrama
+};
