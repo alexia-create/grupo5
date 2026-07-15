0df63d33970271774c59f485ce651bdd9b3ccdcd
ALEXIA_PIETRA <alexia.prado@aluno.uepb.edu.br>
2026-07-15 10:48:38 -0300

    feat: testes iniciais para ver se tá funcionando e retornando os valores esperados

--- index.js
+++ index.js
@@ -21,3 +21,20 @@ function converterMassa(valor, deUnidade, paraUnidade) {
     const valorEmGramas = valor * conversao_em_grama[deUnidade];
     return valorEmGramas / conversao_em_grama[paraUnidade];
 }
+
+//testes
+try {
+    console.log("TESTES DE CONVERSÃO DE MASSA");
+
+    // Teste 1: Converter 1kg para Gramas (Deve dar 1000g)
+    console.log(`1 kg equivale a: ${converterMassa(1, 'kg', 'g')} g`);
+
+    // Teste 2: Converter 500g para Libras (lb)
+    console.log(`500 g equivale a: ${converterMassa(500, 'g', 'lb').toFixed(4)} lb`);
+
+    // Teste 3: Converter 10 Onças (oz) para Quilogramas (kg)
+    console.log(`10 oz equivale a: ${converterMassa(10, 'oz', 'kg').toFixed(4)} kg`);
+
+} catch (error) {
+    console.error("Erro na conversão:", error.message);
+}
