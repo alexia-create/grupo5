703175bef25cda298aa639cdf4efb0cd072149ba
ALEXIA_PIETRA <alexia.prado@aluno.uepb.edu.br>
2026-07-15 10:38:35 -0300

    feat: implementar função principal de conversão

--- index.js
+++ index.js
@@ -7,3 +7,12 @@ const conversao_em_grama = {
     oz: 28.3495,    // Onça
     mg: 0.001       // Miligrama
 };
+
+function converterMassa(valor, deUnidade, paraUnidade) {
+
+    const valorEmGramas = valor * conversao_em_grama[deUnidade];
+
+    const valorConvertido = valorEmGramas / conversao_em_grama[paraUnidade];
+
+    return valorConvertido;
+}
